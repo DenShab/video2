2e420a3d0403c1d3615c80479edce37e0370eb1b
DenShab <didi06@mail.ru>
2018-01-31 21:10:05 +0500

    Init

---
+++ tests/tests/test.java
@@ -0,0 +1,21 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import calculate.Calculator;
+
+public class test {
+
+	private Calculator calculator;
+
+	@Test
+	public void test() {
+		String result; //тестовые параметр
+		calculator = new Calculator();;
+        result = calculator.ReturnCalc(1, 8, 680894 , 1, 7);
+        assertEquals(result, "411,80 ТБ"); //ожидаемый результат
+	}
+
+}
